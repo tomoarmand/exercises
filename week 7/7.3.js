22b81450ca53119d30e37cc1a1ba2e4f9b56651f
tomoarmand <tomo.armand@gmail.com>
2025-03-04 10:00:39 +1300

    a

---
+++ week 7/7.3.js
@@ -0,0 +1,34 @@
+let hello = "Hello, world!"
+console.log(hello);
+
+let greeting = "Hello ";
+let planet = "World";
+console.log(greeting + planet);
+
+let firstName = "James";
+let lastName = "Smith";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+let age = "age ";
+let number = 12;
+let dob = age + number;
+console.log(dob);
+
+let amount = "5";
+let cups = "cups";
+let total = amount + " " + cups;
+console.log(total);
+
+let sentence = `${fullName}, who is ${number}, used ${total} of flour.`;
+console.log(sentence);
+
+
+
+
+
+
+
+
+
+
