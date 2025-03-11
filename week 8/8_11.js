3ed27cd9aa1f301d404c8417ab791550413cfb49
tomoarmand <tomo.armand@gmail.com>
2025-03-11 18:56:13 +1300

    up

--- week 8/8_11.js
+++ week 8/8_11.js
@@ -7,18 +7,18 @@ console.log(doubledNumbers);
 //Use forEach() when you just want to perform an action on each item.
 //Use map() when you want to transform each item and create a new list with the results.
 
-let greaterThanTwo = numbers.filter(element=> {
+let greaterThanTwo = numbers.filter(element => {
     if (element > 2) {
         return true;
     }
 });
 
-let alternative = numbers.filter(element=>element > 2);
+let alternative = numbers.filter(element => element > 2);
 
 console.log(greaterThanTwo);
 console.log(alternative);
 
-let even = numbers.filter(element=> {
+let even = numbers.filter(element => {
     if (element % 2 == 0) {
         return true;
     }
