74bd3e5f71ea2b00e3c38fabfad18d1554a31a3a
tomoarmand <tomo.armand@gmail.com>
2025-04-03 16:37:40 +1300

    up

--- week 11/todo.js
+++ week 11/todo.js
@@ -4,43 +4,88 @@ const inputField = document.getElementById("task-input");
 const addButton = document.getElementById("add-button");
 
 
-const toDoStorage = []
+let toDoStorage = []
 
 
-addButton.addEventListener("click", function() {
+addButton.addEventListener("click", function () {
     addToDo()
 })
 
-function toPage(current) {
+function toPage(current, index) {
     const item = document.createElement("div");
     item.className = "todo-item";
-// Creates new div with a class
-    const radio = document.createElement("input");
-    radio.type = "radio";
-    radio.className = "radio";
-    radio.checked = current.completed;
+    // Creates new div with a class
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.className = "checkbox";
 
-    item.appendChild(radio);
-// Creates a radio button and append it to the div
+    checkbox.checked = current.completed;
+    // .checked is a property of checkbox inputs that determines if they
+    // appear checked or unchecked
+    // current.completed is a boolean: 
+    // if current.completed is false, chickbox will be unchecked
+
+    if (checkbox.checked) {
+        item.classList.add("completed");
+    }// Apply the completed class if the task is already completed
+
+    checkbox.addEventListener("change", function (event) {
+
+        current.completed = checkbox.checked;
+        saveToDos();
+        if (checkbox.checked) {
+            item.classList.add("completed");
+        } else {
+            item.classList.remove("completed");
+        }
+    })
+
+
+
+
+    item.appendChild(checkbox);
+    // Creates a checkbox and append it to the div
     item.appendChild(document.createTextNode(" " + current.title));
-// Adds title text from object
+    // Adds title text from object
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "delete";
+    item.appendChild(deleteButton);
     toDoSection.appendChild(item);
-// Appends item to toDoSection
+    // Appends item to toDoSection
+
+    deleteButton.addEventListener("click", function () {
+        toDoStorage.splice(index, 1);
+        saveToDos();
+        renderToDos();
+        console.log(index);
+        console.log(item);
+        console.log(toDoStorage);
+    })
 };
+
+
 
 function renderToDos() {
     toDoSection.replaceChildren();
-// Updates todo list without repetition by removing old version of list
+    // Updates todo list without repetition by removing old version of list
+    
+    
+    let index = 0;
+    toDoStorage.forEach(function (todo) {
+        console.log(index)
+        toPage(todo, index);
+        index++;
+    // Gets the index for each element of toDoStorage
+    // We need this index to delete from toDoStorage using the splice method
 
-    toDoStorage.forEach(function(todo) {
-        toPage(todo);
     })// Adds latest todo list to page, including latest addition
 };
 
-console.log(toDoStorage)
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    toDoStorage = loadToDos();
     renderToDos();
+
 })
 
 function addToDo() {
@@ -52,10 +97,24 @@ function addToDo() {
 
     toDoStorage.push(newToDo);
 
+    saveToDos();
+
     inputField.value = "";
 
     renderToDos()
 };
+
+function saveToDos() {
+    localStorage.setItem("toDoStorage", JSON.stringify(toDoStorage));
+};
+
+function loadToDos() {
+    const savedToDos = localStorage.getItem("toDoStorage");
+    if (savedToDos) {
+        return JSON.parse(savedToDos);
+    }
+    return [];
+}
 
 
 
